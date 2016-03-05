d926b48dfa4860fe7481eeef784fdcff322b6c21
Benny Franco Dennis <bennyfranco@sandcode.com>
2016-03-05 16:19:18 -0700

    Text file reader line by line implemented

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    string headerLine;
+    bool isFirstLine=true;
+    ifstream inFile("../../data/Jazmin/20150201.txt");
+    if (!inFile) {
+        cerr << "File not found." << endl;
+        return -1;
+    }
+
+    // Using getline() to read one line at a time.
+    string line;
+    while (getline(inFile, line)) {
+        if (line.empty()) continue;
+        if(isFirstLine){
+            headerLine = line;
+            isFirstLine=false;
+        }
+        //cout<<line<<endl;
+    }
+
+    inFile.close();
+
+    cout<<headerLine<<endl;
     return 0;
 }
