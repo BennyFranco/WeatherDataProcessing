f02f58919e74e052b4ccf0c7eab3d78348ded337
Benny Franco Dennis <bennyfranco@sandcode.com>
2016-03-05 20:04:56 -0700

    Processing data and create mongo documents implemented

--- main.cpp
+++ main.cpp
@@ -3,16 +3,34 @@
 #include <sstream>
 #include <vector>
 
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+
 using namespace std;
 
 int main() {
 
+    mongocxx::instance inst{};
+    mongocxx::client conn{mongocxx::uri{"mongodb://127.0.0.1/ClimaPIEAES?"}};
+
+    auto collection = conn["ClimaPIEAES"]["JAZMIN"];
+
     string headerLine;
     bool isFirstLine=true;
-    std::vector<string> a;
-    std::vector<string>::const_iterator i;
+
+    vector<string> headersVector;
+    vector<string> dataLineVector;
+
+    //vector<string>::const_iterator i;
+    vector<string>::const_iterator j;
+    vector<string>::const_iterator k;
 
     ifstream inFile("../../data/Jazmin/20150201.txt");
+
+
     if (!inFile) {
         cerr << "File not found." << endl;
         return -1;
@@ -25,26 +43,62 @@ int main() {
         if(isFirstLine){
             headerLine = line;
             isFirstLine=false;
+        }else {
+            dataLineVector.push_back(line);
         }
-        //cout<<line<<endl;
     }
 
     inFile.close();
 
+
     string delimiter = ",";
     size_t pos = 0;
     string token;
-    while ((pos = headerLine.find(delimiter)) != std::string::npos) {
+    while ((pos = headerLine.find(delimiter)) != string::npos) {
         token = headerLine.substr(0, pos);
-       // std::cout << token << std::endl;
+        token.replace(token.begin(),token.begin()+1,"");
+        token.replace(token.end()-1,token.end(),"");
         headerLine.erase(0, pos + delimiter.length());
-        a.push_back(token);
+        headersVector.push_back(token);
     }
-    a.push_back(headerLine);
-    //std::cout << headerLine << std::endl;
+    headerLine.replace(headerLine.begin(),headerLine.begin()+1,"");
+    headerLine.replace(headerLine.end()-1,headerLine.end(),"");
+    headerLine.replace(headerLine.end()-1,headerLine.end(),"");
+    headersVector.push_back(headerLine);
 
-    for(i=a.begin(); i!=a.end(); ++i){
-        std::cout<<(*i)<<std::endl;
+        for(j=dataLineVector.begin(); j!=dataLineVector.end(); ++j){
+            string temp = (*j);
+            vector<string> dataTempVector;
+            while ((pos = temp.find(delimiter)) != string::npos) {
+                token = temp.substr(0, pos);
+                temp.erase(0, pos + delimiter.length());
+                token.replace(token.begin(),token.begin()+1,"");
+                token.replace(token.end()-1,token.end(),"");
+                dataTempVector.push_back(token);
+            }
+
+            temp.replace(temp.begin(),temp.begin()+1,"");
+            temp.replace(temp.end()-1,temp.end(),"");
+            temp.replace(temp.end()-1,temp.end(),"");
+            dataTempVector.push_back(temp);
+
+            /*for(i=headersVector.begin(),k=dataTempVector.begin(); k!=dataTempVector.end(); ++i,++k) {
+                document << headersVector.at(k) << (*k);
+            }*/
+
+            bsoncxx::builder::stream::document document{};
+
+            for(std::size_t i=0;i<dataTempVector.size();++i){
+                document << headersVector[i] << dataTempVector[i];
+            }
+            collection.insert_one(document.view());
+        }
+
+    //cout << headersVector.size() << endl;
+    auto cursor = collection.find({});
+
+    for (auto&& doc : cursor) {
+        std::cout << bsoncxx::to_json(doc) << std::endl;
     }
 
     return 0;
